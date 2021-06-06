55efaa8ae00a31fd8035b907d99f57eae714ea0f
Richter-Hoff <richterhoff@gmail.com>
2021-06-06 19:18:29 +0300

    create: added slider

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ menuButton.addEventListener("click", function () {
     .querySelector(".navbar-menu")
     .classList.toggle("navbar-menu--visible");
 });
+
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button slider-button--next",
+    prevEl: ".slider-button slider-button--prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
